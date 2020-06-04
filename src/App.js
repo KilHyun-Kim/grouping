9ef59fdd6dd98150b0d331c8882b8d90faf72524
KilHyun-Kim <rlaarlfgus@gmail.com>
2020-06-04 22:09:08 +0900

    Create a button that enables deletion and modification when loggin in

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import JobPage from "./pages/JobPage";
 import WritePage from "./pages/WritePage";
 import PostPage from "./pages/PostPage";
 import PostListPage from "./pages/PostListPage";
-
+import { Helmet } from "react-helmet-async";
 function App() {
   return (
     // router 간단하게 표시하는법 다시
     <>
       {/* <Route component={MainPage} path="/" exact /> */}
+      <Helmet>
+        <title>GroupingPong</title>
+      </Helmet>
       <Route component={WebPage} path="/web" exact />
       <Route component={JobPage} path="/web/designer" />
       <Route component={PostPage} path="/@:username/:postId" />
--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -6,9 +6,13 @@ import EditorContainer from "../containers/write/EditorContainer";
 import TagBoxContainer from "../containers/write/TagBoxContainer";
 import WriteActionButtonsContainer from "../containers/write/WriteActionButtonsContainer";
 import Table from "../components/write/Table";
+import { Helmet } from "react-helmet-async";
 const WritePage = () => {
   return (
     <>
+      <Helmet>
+        <title>GroupingPong - 글쓰기</title>
+      </Helmet>
       <HeaderContainer />
       <Responsive>
         <Subject subject={"웹 디자이너 스터디 그룹원 모집"} />
